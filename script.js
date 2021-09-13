bd6e457bc533ce99ec1c7ddbb5725ebb13866f2a
pradnya237 <pradnya237@solacetechnologies.co.in>
2021-09-13 10:31:49 +0530

    form validation

--- script.js
+++ script.js
@@ -1,67 +1,103 @@
- console.log("Connected to js file");
-// function saveData(event) {
-// 	event.preventDefault();
-// 	//event.preventDefault();
-// 	// let fname,lname,gender,DOB,country,email,contact,query;
-// 	// fname=document.getElementById("fname").value
-// 	// lname=document.getElementById("lname").value
-// 	// gender1=document.getElementById("Female").value
-// 	// gender2=document.getElementById("Male").value
-// 	// DOB=document.getElementById("DOB").value
-// 	// country=document.getElementById("country").value
-// 	// email=document.getElementById("Email").value
-// 	// contact=document.getElementById("tel").value
-// 	// query=document.getElementById("Question").value
-
-// 	// localStorage.setItem("fname",fname);
-// 	// localStorage.setItem("lname",lname);
-// 	// localStorage.setItem("gender",gender1);
-// 	// localStorage.setItem("gender",gender2);
-// 	// localStorage.setItem("DOB",DOB);
-// 	// localStorage.setItem("country",country);
-// 	// localStorage.setItem("email",email);
-// 	// localStorage.setItem("contact",contact);
-// 	// localStorage.setItem("query",query);
-// 	//document.querySelectorAll('input').value;
-	 var obj=Array.from(document.querySelectorAll('input')).reduce((acc,input)=>({
-	 	...acc,[input.id] :input.value}),
-	 {});
-	 console.log(obj);
- // }
+console.log("Connected to js file");
 let personData = [];
-        const addPerson = (event)=>{
-            event.preventDefault();  //to stop the form submitting
-            let person = {
-                date: Date(),
-                fristName: document.getElementById('fname').value,
-                lastName: document.getElementById('lname').value,
-                gender:document.getElementById('gender').value,
-                DOB:document.getElementById('DOB').value,
-                Country:document.getElementById('country').value,
-                email:document.getElementById('Email').value,
-                contactNumber:document.getElementById('tel').value,
-                query:document.getElementById('my-textarea').value
+ const addPerson = (event)=>{
+ 	    //to stop the form submitting
+	event.preventDefault(); 
+   	if(document.getElementById('fname').value!="" &&
+   		document.getElementById('lname').value!=""&&
+   		document.getElementById('Email').value!="")
+   	{
+   	//collecting gender value
 
-            }
-            personData.push(person);
-            //document.forms[0].reset();
-             // to clear the form for the next entries
-           // document.querySelector('form').reset();
+    var ele = document.getElementsByName('Gender');
+	var val=''
+	for(i = 0; i < ele.length; i++) {
+	if(ele[i].checked)
+	val=ele[i].value;}
+    let person = {
+    	date: Date(),
+    	fristName: document.getElementById('fname').value,
+    	lastName: document.getElementById('lname').value,
+    	gender:val,
+    	DOB:document.getElementById('DOB').value,
+    	Country:document.getElementById('country').value,
+    	email:document.getElementById('Email').value,
+    	contactNumber:document.getElementById('tel').value,
+    	query:document.getElementById('Question').value}
+    personData.push(person);
+    console.log(personData);
+	//saving to localStorage
+    localStorage.setItem('MypersonList', JSON.stringify(personData) );
+    myReset();
+    alert("form Submited Succefully!")
+     // document.querySelector('form').reset();
 
-            console.log(personData)
+    }
+    else 
+    { var x;
+    		  x = document.querySelectorAll(".form-input");
+    		  for (i = 0; i < x.length; i++){
+    		  	
+    		  	if(x[i]==document.getElementById('fname')&&
+    		  		document.getElementById('fname').value=="")
+    		  	{x[i].style.border = "1.5px solid #f72c1e";}
+    		     else if(document.getElementById('fname').value!="")
+    		  	{x[i].style.backgroundColor = "1.5px solid #1bf21";}
 
-            //saving to localStorage
-            localStorage.setItem('MypersonList', JSON.stringify(personData) );
-        }
-        document.addEventListener('DOMContentLoaded', ()=>{
-            document.getElementById('submit').addEventListener('click', addPerson);
-        });
-      const ackg =(event)=>{
+
+    		  	if(x[i]==document.getElementById('lname')&&
+    		  		document.getElementById('lname').value=="")
+    		  	{x[i].style.border = "1.5px solid #f72c1e";}
+    		  else if(document.getElementById('lname').value!="")
+    		  	{x[i].style.border = "1.5px solid #1bf21b";}
+
+    		  if(x[i]==document.getElementById('DOB')&&
+    		  		document.getElementById('DOB').value=="")
+    		  	{x[i].style.border = "1.5px solid black";}
+    		  else if(document.getElementById('lname').value!="")
+    		  	{x[i].style.border = "1.5px solid #1bf21b";}
+
+    		  if(x[i]==document.getElementById('country')&&
+    		  		document.getElementById('country').value=="")
+    		  	{x[i].style.border = "1.5px solid black";}
+    		  else if(document.getElementById('country').value!="")
+    		  	{x[i].style.border = "1.5px solid #1bf21b";}
+
+			if(x[i]==document.getElementById('Email')&&
+    		  		document.getElementById('Email').value=="")
+    		  		  {x[i].style.border = "1.5px solid #f72c1e";}
+    		  		else if(document.getElementById('Email').value!="")
+    		  	{x[i].style.border = "1.5px solid #1bf21b";}
+
+    		  if(x[i]==document.getElementById('tel')&&
+    		  		document.getElementById('tel').value=="")
+    		  		{x[i].style.border = "1.5px solid #f72c1e";}
+    		  	else if(document.getElementById('tel').value!="")
+    		  	{x[i].style.border = "1.5px solid #1bf21b";}
 
 
-      	alert('Form Submitted!')
-document.getElementById('reg-form-input').reset();
+    		   if(x[i]==document.getElementById('Question')&&
+    		  		document.getElementById('Question').value=="")
+    		  		{x[i].style.border = "1.5px solid black";}
+    		  	else if(document.getElementById('Question').value!="")
+    		  	{x[i].style.border = "1.5px solid #1bf21b";} }
+    		  			
+	 	
+	}}
 
+	// Adding event lisrner on loading doccument
+         document.addEventListener('DOMContentLoaded', ()=>{
+     	document.getElementById('submit').addEventListener('click', addPerson);
+     });
+
+	
+   const myReset=()=>{
+   	     document.querySelector('form').reset();
+   	     var x, i;
+  x = document.querySelectorAll(".form-input");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.border = "1.5px solid black";
   }
-        	      // HTMLFormElement.reset()
+};
+
 
